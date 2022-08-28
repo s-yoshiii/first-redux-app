2bb87544970fc9469dc87ecfe28e52d0ce58aa85
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-08-28 16:56:59 +0900

    Store,Action,Reducerを実際にコーディングして準備しよう

--- src/index.js
+++ src/index.js
@@ -2,21 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import {createStore} from 'redux';
 // actions => increment,decrement
-
-// reducer
+const increment = () => {
+  return {
+    type:'INCREMENT',
+  }
+}
+const decrement = () => {
+  return {
+    type:'DECREMENT',
+  }
+}
+// reducer => アクションと前の状態を組み合わせて新しい状態に更新する。
+const counterReducer = (state = 0,action) =>{
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+        return state;
+  }
+}
 
 // store
+let store = createStore(counterReducer)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
