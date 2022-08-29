343602fb72b1cd949596a869ef077d12f199af23
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-08-29 23:56:29 +0900

    複数のReducerを組み合わせる操作を始めよう

--- src/index.js
+++ src/index.js
@@ -3,27 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
-// actions => increment,decrement
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
+import allReducers from "./reducers";
 // store
-let store = createStore(counterReducer);
-
-// コンソールに新しい状態を出力
-store.subscribe(() => console.log(store.getState()));
-// dispatch
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
+const store = createStore(allReducers);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
