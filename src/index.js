b2dfa4856ce4530a5c946009d3e6e02243fa424e
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-08-29 09:48:04 +0900

    Dispatchで通知して新しい状態になったらログを出力してみよう

--- src/index.js
+++ src/index.js
@@ -1,35 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {createStore} from 'redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
 // actions => increment,decrement
 const increment = () => {
   return {
-    type:'INCREMENT',
-  }
-}
+    type: "INCREMENT",
+  };
+};
 const decrement = () => {
   return {
-    type:'DECREMENT',
-  }
-}
+    type: "DECREMENT",
+  };
+};
 // reducer => アクションと前の状態を組み合わせて新しい状態に更新する。
-const counterReducer = (state = 0,action) =>{
-  switch(action.type){
-    case 'INCREMENT':
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
       return state + 1;
-    case 'DECREMENT':
+    case "DECREMENT":
       return state - 1;
     default:
-        return state;
+      return state;
   }
-}
+};
 
 // store
-let store = createStore(counterReducer)
+let store = createStore(counterReducer);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// コンソールに新しい状態を出力
+store.subscribe(() => console.log(store.getState()));
+// dispatch
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
