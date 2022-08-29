a5ba8978c5a4fc55f2a644f9a41fc350799b59c2
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-08-30 00:10:19 +0900

    ProviderでグローバルにStoreを利用してみよう

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 import allReducers from "./reducers";
+import { Provider } from "react-redux";
 // store
 const store = createStore(
   allReducers,
@@ -13,6 +14,8 @@ const store = createStore(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
