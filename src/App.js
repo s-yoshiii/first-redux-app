d260b0f48232a452d5d9df5134a9ef6a309910ee
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-08-30 09:45:03 +0900

    useSelectorのHooksを使って状態にアクセスしよう

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
-
+import "./App.css";
+import { useSelector } from "react-redux";
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogin = useSelector((state) => state.isLogin);
   return (
     <div className="App">
-     <h1>Hello Redux</h1>
+      <h1>Hello Redux</h1>
+      <h3>カウント：{counter}</h3>
+      {isLogin ? <h3>ログインに成功！</h3> : <h3>ログインして下さい</h3>}
     </div>
   );
 }
