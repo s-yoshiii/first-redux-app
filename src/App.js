ad8222e1393e8b74887cca2e71c204c08941deaf
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-08-31 00:15:59 +0900

    payloadを追加して7ずつカウントアップさせよう

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <h1>Hello Redux</h1>
       <h3>カウント：{counter}</h3>
-      <button onClick={() => dispatch(increment())}>＋</button>
+      <button onClick={() => dispatch(increment(7))}>＋</button>
       <button onClick={() => dispatch(decrement())}>－</button>
       {isLogin ? <h3>ログインに成功！</h3> : <h3>ログインして下さい</h3>}
       <button onClick={() => dispatch(login())}>ログインorログアウト</button>
